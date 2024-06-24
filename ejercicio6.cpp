261e8f64acd10d5acaf99694238ea1a9a60657cc
lfcondoric <lfcondoric@unjbg.edu.pe>
2024-06-24 18:13:46 -0500

    Añadiendo ejercicio6.cpp al repositorio

---
+++ ejercicio6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+void numeroAL(int a, int b){
+	srand(time(0));
+	for(int i=1;i<=a;i++){
+		int numero=+rand()%(b+1-0);
+		cout<<numero<<", ";
+	}
+}
+int main(){
+	int c,m;
+	cout<<"Ingrese la cantidad de numero que desea generar: ";cin>>c;
+	cout<<"Ingrese el maximo numero permitido: ";cin>>m;
+	numeroAL(c,m);	
+}
